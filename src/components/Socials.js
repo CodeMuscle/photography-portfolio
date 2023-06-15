7e6417230148f2f1ade0e90746567c30c42ae66d
Ganesh Govind J <codemuscle@Ganeshs-MacBook-Air.local>
2023-06-15 11:32:28 +0530

    Website complete

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+import {
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+} from 'react-icons/im';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex flex-row gap-x-4">
+        <li>
+          <a href="www.facebook.com" target="_blank">
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a href="www.instagram.com" target="_blank">
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="www.pinterest.com" target="_blank">
+            <ImPinterest />
+          </a>
+        </li>
+        <li>
+          <a href="www.twitter.com" target="_blank">
+            <ImTwitter />
+          </a>
+        </li>
+      </ul>
+    </div>
+  
+  )
 };
 
 export default Socials;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,56 @@
 import React from 'react';
 
+import WomanImg from '../img/about/woman.png';
+
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { transition1 } from '../transitions'
+
+
+
 const About = () => {
-  return <div>About</div>;
+  return (
+  
+    <motion.section 
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={transition1}
+    className="section">
+      <div className="container mx-auto h-full relative">
+        {/* Text & img */}
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-44">
+          {/* image */}
+          <motion.div
+          initial={{ opacity: 0, y: '100%' }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y:'100%' }}
+          transition={transition1}
+          className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <img src={WomanImg} alt="about-img" />
+          </motion.div>
+          {/* text */}
+          <motion.div
+          initial={{ opacity: 0, y: '-80%' }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y:'-80%' }}
+          transition={transition1}
+          className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start ">
+            <h1 className="h1">About Me</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam cumque explicabo dolorem alias asperiores provident at, <b>nostrum laudantium</b> impedit similique.
+              <br/>
+              <br/>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, eos voluptate fuga voluptatem possimus consectetur.
+            
+            </p>
+            <Link to={'/portfolio'} className="btn">View My Work</Link>
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
+  
+  )
 };
 
 export default About;
